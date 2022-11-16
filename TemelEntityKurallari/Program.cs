9bd07005cfb71f657ec698474c346f0514bffce3
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-11-16 06:34:34 +0300

    .

--- TemelEntityKurallari/Program.cs
+++ TemelEntityKurallari/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,6 +25,7 @@ namespace TemelEntityKurallari
             //};
             //await context.AddAsync(product1);
             //await context.Products.AddAsync(product);
+            //await context.SaveChangesAsync();
             #endregion
 
             #region SaveChanges nedir 
@@ -54,19 +56,18 @@ namespace TemelEntityKurallari
             //    ProductName = "C ürünü",
             //    UnitPrice = 25.54
             //};
-            //Console.WriteLine(context.Entry(product1));
 
-            //// TEK TEK ekledik
-            ////await context.AddAsync(product1); 
-            ////await context.AddAsync(product2);
-            ////await context.AddAsync(product3);
+            // TEK TEK ekledik
+            //await context.AddAsync(product1); 
+            //await context.AddAsync(product2);
+            //await context.AddAsync(product3);
 
             //await context.Products.AddRangeAsync(product1, product2, product3); // Hepsini birden ekledik.
             //await context.SaveChangesAsync();
-            ////Savechanges fonksiyonu her tetiklendiğinde bir transaction oluşturacağından dolayı Ef core ile yapılan her bir işleme özel kullanmaktan kaçınmalıyız! Çünkü her işleme
-            //// özel transaction veritabanı açısından ekstra maliyet demektir. O yüzden mümkün mertebe tüm işlerimizi tek bir transaction eşliğinde veritabanına gönderebilmek için
-            //// savechanges'i aşağıdaki gibi tek seferde kullanmak hem maliyet hem de yönetilebilebilirlik açısından katkıda bulunmuş 
-            //Console.WriteLine(context.Entry(product1)); // Her bir işleme özel sorguyu tek bir transactionda oluşturup bunu veritabanına gönderir.
+            //Savechanges fonksiyonu her tetiklendiğinde bir transaction oluşturacağından dolayı Ef core ile yapılan her bir işleme özel kullanmaktan kaçınmalıyız! Çünkü her işleme
+            // özel transaction veritabanı açısından ekstra maliyet demektir. O yüzden mümkün mertebe tüm işlerimizi tek bir transaction eşliğinde veritabanına gönderebilmek için
+            // savechanges'i aşağıdaki gibi tek seferde kullanmak hem maliyet hem de yönetilebilebilirlik açısından katkıda bulunmuş 
+            // Her bir işleme özel sorguyu tek bir transactionda oluşturup bunu veritabanına gönderir.
 
 
             #endregion
@@ -91,14 +92,14 @@ namespace TemelEntityKurallari
             // ChangeTracker mekanizması tarafından takip edilmeyen nesnelerin güncellenebilmesi için Update fonksiyonu kullnılır!
             // Update fonksiyonunu kullanabilmek için kesinlikle ilgili nesnede id bilgisi verilmelidir.
             // Bu değer güncellenecek(update sorgusu oluşturulacak) verinin hangisi olduğunu ifade edecektir.
-            ECommerceContext context = new();
-            Product product1 = new()
-            {
-                ProductId = 3,
-            };
-            product1.ProductName = "C usdfdsf.";
-            context.Products.Update(product1);
-            await context.SaveChangesAsync();
+            //ECommerceContext context = new();
+            //Product product1 = new()
+            //{
+            //    ProductId = 3,
+            //};
+            //product1.ProductName = "C usdfdsf.";
+            //context.Products.Update(product1);
+            //await context.SaveChangesAsync();
             #endregion
 
             #region EntityState Nedir?
@@ -118,18 +119,48 @@ namespace TemelEntityKurallari
 
             #region Birden fazla veri güncellenirken nelere dikkat edilmelidir?
 
-            var products = await context.Products.ToListAsync(); // chanfe tracker takip eder
-            foreach (var item in products)
-            {
-                item.ProductName += "*";
-            }
-            await context.SaveChangesAsync();
-            foreach (var item in products)
-            {
-                Console.WriteLine(item.ProductName);
-            }
+            //var products = await context.Products.ToListAsync(); // chanfe tracker takip eder
+            //foreach (var item in products)
+            //{
+            //    item.ProductName += "*";
+            //}
+            //await context.SaveChangesAsync();
+            //foreach (var item in products)
+            //{
+            //    Console.WriteLine(item.ProductName);
+            //}
             #endregion
 
+            #region Veri Nasıl Silinir?
+            //Product product = await context.Products.SingleOrDefaultAsync(p => p.ProductId == 3);
+            //context.Products.Remove(product);
+            //await context.SaveChangesAsync();
+            // Veri context'ten gelmezse?
+            //ECommerceContext context = new();
+            //Product product = new()
+            //{
+            //    ProductId = 2
+            //};
+            //context.Products.Remove(product);
+            //await context.SaveChangesAsync();
+
+            #endregion
+            #region EntityState ile Silme İşlemi
+            //ECommerceContext context = new();
+            //Product product = new()
+            //{ ProductId = 4 };
+            //context.Entry(product).State = EntityState.Deleted;
+            //await context.SaveChangesAsync();
+            #endregion
+            #region Birden fazla veri silinirken nelere dikkat edilmeli?
+            // Ne kadar veri sileceksen sil, savechanges'i bir defa çalıştır. Savechanges'i verimli kullan.
+            #endregion
+            #region RemoveRange
+            //ECommerceContext context = new();
+            //List<Product> products =  context.Products.Where(p => p.ProductId >= 9 && p.ProductId <= 11).ToList();
+            //context.Products.RemoveRange(products);
+            //await context.SaveChangesAsync();
+            #endregion
         }
     }
 }
